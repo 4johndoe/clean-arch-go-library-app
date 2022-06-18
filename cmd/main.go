bdec6829ddd43d85b8aed142de56d3cad4ea521d
bogdan demchenko <bogdandemchenko@gmail.com>
2022-06-18 14:58:10 +0300

    upd

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"ca-library-app/internal/composites"
+	"ca-library-app/internal/config"
+	"ca-library-app/pkg/logging"
+	"context"
 	"fmt"
 )
 
 func main() {
-	// entry point
-	authorComposite, err := composites.NewAuthorComposite()
+	logging.Init()
+	logger := logging.GetLogger()
+
+	logger.Info("config initializing")
+	cfg := config.GetConfig()
+
+	logger.Info("mongo composite initializing")
+	mongoDBC, err := composites.NewMongoDBComposite(context.Background(), "", "", "", "", "", "")
 	if err != nil {
+		logger.Fatal("mongodb composit failed")
 	}
 
-	bookComposite, err := composites.NewBookComposite()
+	logger.Info("author composite initializing")
+	authorComposite, err := composites.NewAuthorComposite(mongoDBC)
 	if err != nil {
+		logger.Fatal("author composite failed")
+	}
+
+	logger.Info("book  composite initializing")
+	bookComposite, err := composites.NewBookComposite(mongoDBC)
+	if err != nil {
+		logger.Fatal("book composite failed")
 	}
 
 	// register in router
-	fmt.Println(authorComposite, bookComposite)
+	fmt.Println(authorComposite, bookComposite, cfg)
 }
